2a4ae3d00ec42c0e45560b464d38cdd45ce47c79
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-01-06 15:06:14 -0800

    add regex to string input for name and type

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -8,6 +8,7 @@ namespace Factory.Models
     public int EngineerId { get; set; }
     [Required(ErrorMessage="Name must be added for new Engineer.")]
     [StringLength(60,ErrorMessage="No more than 60 characters")]
+    [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Name must only include characters.")]
     [Display(Name="Engineer Name")]
     public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,6 +8,7 @@ namespace Factory.Models
     public int MachineId { get; set; }
     [Required(ErrorMessage="Machine type must be filled to setup new machine.")]
     [StringLength(60,ErrorMessage="No more than 60 characters")]
+    [RegularExpression(@"^[a-zA-Z].*$", ErrorMessage = "Type must start with character.")]
     [Display(Name="Machine Type")]
     public string Type { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
